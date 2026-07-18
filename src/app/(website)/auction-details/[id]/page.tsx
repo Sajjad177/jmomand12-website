01ce88bd9f0f803fac7e903a998f63069b7ca2e7
jarifMohammed <mohammedjarif@protonmail.com>
2026-07-18 22:49:49 +0600

    design card design fixed

--- src/app/(website)/auction-details/[id]/page.tsx
+++ src/app/(website)/auction-details/[id]/page.tsx
@@ -1,12 +1,12 @@
-import AuctionProductDetailsPage from '@/features/auction-site/components/auctions-details-page'
-import React from 'react'
+import AuctionProductDetailsPage from "@/features/auction-site/components/auctions-details-page";
+import React from "react";
 
 const page = () => {
   return (
     <div>
- <AuctionProductDetailsPage/>
+      <AuctionProductDetailsPage />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
